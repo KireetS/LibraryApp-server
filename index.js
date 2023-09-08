67a012eccc5ed0b0ee2234348755a0ce1ab6ef9c
Kireet <singhkireet5@gmail.com>
2023-09-08 10:44:13 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require("express");
 const connectToMongo = require("./db")
+const cors = require("cors")
 
 
 const app = express();
 connectToMongo()
 app.use(express.json())
+app.use(cors())
 app.use("/api/auth" , require("./routes/auth"))
 app.use("/api/books" , require("./routes/books"))
 app.get("/",(req,res)=>{
